ed579af27e93e03111e50da747806088f51da8b9
Ivo-Donchev <ivodonchev267@gmail.com>
2018-02-15 23:48:16 +0200

    Implement websockets at the server and listen to the client

--- server/app.js
+++ server/app.js
@@ -7,53 +7,110 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var games = require('./routes/games');
 
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+
+  // Request methods you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/games', games);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+/// Sockets stuff
+
+io.on('connection', socket => {
+  console.log('user connected successfully');
+  socket.emit('connected');
+
+  socket.on('disconnect', () => {
+    io.emit('userDisconnected', {socketId: socket.id});
+  });
+
+  socket.on('updateValues', data => {
+    console.log('updating values');
+
+    const values = {data};
+    io.emit('updateValues', {socketId: socket.id, value: values});
+  });
+
+  socket.on('newGameStarted', data => {
+    console.log('new game started');
+
+    const values = {data};
+    io.emit('newGameStarted', {socketId: socket.id, value: values});
+  });
+
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+});
+
+console.log('starting server on port 3001');
+io.listen('3001');
 
 /// error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
-
 
 module.exports = app;
